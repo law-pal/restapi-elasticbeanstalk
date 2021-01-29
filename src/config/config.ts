38baef655ab553a0465f3d5b47b4817496622486
law-pal <lawrencepalacios1@gmail.com>
2021-01-29 09:31:43 -0600

    fixed aws_config

--- src/config/config.ts
+++ src/config/config.ts
@@ -8,7 +8,7 @@ export const config = {
     "host": process.env.POSTGRES_HOST,
     "dialect": "postgres",
     "aws_region": process.env.AWS_REGION,
-    "aws_profile": process.env.AWS_REGION,
+    "aws_profile": process.env.AWS_PROFILE,
     "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
   },
   "prod": {
